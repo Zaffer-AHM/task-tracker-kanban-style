80fb5fe95db4a6887cf2a62ee455a968963a7836
Zaffer Ahmed <56196264+Zaffer-AHM@users.noreply.github.com>
2026-01-24 04:19:09 +0530

    deleting old backend
    + redesigning new one, after learning better backend improvements
    
    - need to add system design in readme
    - frontend sample design

--- task-tracker/src/main/java/com/task/app/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.task.app.entity.Role;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
-}
--- task-tracker/src/main/java/com/task/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.task.app.entity.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
