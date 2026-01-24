8777ff1aac3d5b860c2e5c8ffd7849db3ce372b7
Zaffer-AHM <zafferahm@gmail.com>
2026-01-24 23:54:08 +0530

    initialized v2 backend and frontend

---
+++ backend/src/test/java/tasktracker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tasktracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
